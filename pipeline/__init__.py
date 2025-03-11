151faa173b34b3a1db8cc53c686e4c6ec3f194ae
hakkiir <hakkaraineniiro@gmail.com>
2025-03-11 17:11:32 +0200

    linting

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -6,3 +6,12 @@ from .nhl_data_workflow import (
     create_nhl_teams_pipeline,
     create_nhl_current_standings_pipeline
 )
+
+__all__ = [
+'create_nhl_franchise_pipeline,'
+'create_nhl_roster_pipeline,'
+'create_nhl_schedule_backfill_pipeline,'
+'create_nhl_teams_pipeline,'
+'create_nhl_current_standings_pipeline',
+'insert_divisions'
+]
